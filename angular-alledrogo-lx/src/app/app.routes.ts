065204bf929cb0dd7b9b4ee2b97821ddecd3423a
akopij70 <73650280+akopij70@users.noreply.github.com>
2024-05-09 15:28:38 +0200

    Add homepage with navbar

--- angular-alledrogo-lx/src/app/app.routes.ts
+++ angular-alledrogo-lx/src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomepageComponent },
+  { path: 'login', component: HomepageComponent },
+  { path: 'account', component: HomepageComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
