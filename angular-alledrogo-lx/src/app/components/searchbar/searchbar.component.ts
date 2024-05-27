58d84476d3aeb38dc4516010e714102164577b6c
akopij70 <73650280+akopij70@users.noreply.github.com>
2024-05-27 16:05:51 +0200

    add most popluar tag hint

--- angular-alledrogo-lx/src/app/components/searchbar/searchbar.component.ts
+++ angular-alledrogo-lx/src/app/components/searchbar/searchbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChildren, QueryList, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Tag } from '../../models/tag';
 import { ActivatedRoute, RouterLink } from '@angular/router';
@@ -25,6 +25,39 @@ export class SearchbarComponent {
     this.tags = this.activatedRoute.snapshot.data['tags']
   }
 
+  onInputChange(searchString: string): void {
+    const hintBtn = document.getElementsByClassName('hint-tag-button') as HTMLCollectionOf<HTMLElement>;
+    hintBtn[0].style.visibility = "hidden";
+    this.tagHint = '';
+    if (searchString.startsWith('#')) {
+      const searchTag = searchString.substring(1);
+      const matchingTags = this.tags.filter(tag => tag.name.startsWith(searchTag));
+      hintBtn[0].style.visibility = "visible";
+      if (matchingTags.length > 0) {
+        const bestTag = matchingTags.reduce((prev, current) => (prev.postCount > current.postCount) ? prev : current);
+        this.tagHint = bestTag ? bestTag.name : '';
+      }
+      else {
+        const bestTag = this.tags.reduce((prev, current) => (prev.postCount > current.postCount) ? prev : current);
+        this.tagHint = bestTag.name;
+      }
+    }
+  }
+
+  onHintTagClick(){
+    const hint = this.tagHint;
+    const checkbox = document.getElementById(hint) as HTMLInputElement;
+    if (checkbox) {
+      if (!checkbox.checked) {
+        checkbox.checked = true;
+        if (!this.selectedTags.includes(hint)) {
+          this.selectedTags.push(hint);
+        }
+      }
+    }
+    console.log(this.selectedTags);
+  }
+
   onTagChange(tag: string, event: any) {
     if (event.target.checked) {
       this.selectedTags.push(tag);
@@ -38,9 +71,15 @@ export class SearchbarComponent {
   }
 
   searchByString(searchString: string) {
+    let bodySearch: string = '';
+    if (searchString.startsWith('#')) {
+      if (searchString.includes(' ')) {
+        bodySearch = searchString.substring(searchString.indexOf(' ') + 1);
+      }
+    }
     const body = {
       search: {
-        queryString: searchString,
+        queryString: bodySearch,
         tags: []
       }
     }
@@ -48,9 +87,15 @@ export class SearchbarComponent {
   }
 
   searchAll(searchString: string) {
+    let bodySearch: string = '';
+    if (searchString.startsWith('#')) {
+      if (searchString.includes(' ')) {
+        bodySearch = searchString.substring(searchString.indexOf(' ') + 1);
+      }
+    }
     const body = {
       search: {
-        queryString: searchString,
+        queryString: bodySearch,
         tags: this.selectedTags
       }
     }
