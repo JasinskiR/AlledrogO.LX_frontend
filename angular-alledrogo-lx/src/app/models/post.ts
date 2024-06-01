300d7e02404283eb7f4cb61055a3bfdf5120f80d
akopij70 <73650280+akopij70@users.noreply.github.com>
2024-06-01 16:38:38 +0200

    repair showing images in details

--- angular-alledrogo-lx/src/app/models/post.ts
+++ angular-alledrogo-lx/src/app/models/post.ts
@@ -1,9 +1,15 @@
+interface Image {
+  id: string;
+  url: string;
+  isMain: boolean;
+}
+
 export interface Post {
   id: string;
   title: string;
   description?: string;
   image?: string;
-  images?: string[];
+  images?: Image[];
   tags?: string[];
   status?: number;
   authorId?: string;
