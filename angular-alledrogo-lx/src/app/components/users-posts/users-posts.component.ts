c055dea9c53797c2c63786563c688fdb2024ef0f
akopij70 <73650280+akopij70@users.noreply.github.com>
2024-06-07 14:40:06 +0200

    Add showing posts created by user

--- angular-alledrogo-lx/src/app/components/users-posts/users-posts.component.ts
+++ angular-alledrogo-lx/src/app/components/users-posts/users-posts.component.ts
@@ -1,20 +1,35 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import {AuthService} from "../../services/auth.service";
+import { Post } from '../../models/post';
+import { PostInGeneralComponent } from '../post-in-general/post-in-general.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-users-posts',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    PostInGeneralComponent,
+    RouterLink
+  ],
   templateUrl: './users-posts.component.html',
   styleUrl: './users-posts.component.scss'
 })
 export class UsersPostsComponent {
-  constructor (private authService: AuthService, private router: Router) {}
+  allPosts: Post[] = [];
+
+  constructor (private activatedRoute: ActivatedRoute, private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
     if (!this.authService.isLoggedIn) {
       this.router.navigate(['/home']);
     }
+    this.activatedRoute.data.subscribe(data => {
+      this.allPosts = data['posts'];
+    });
+    console.log(this.allPosts);
   }
 }
