c512ee7f76286df86903871506c6cec1b2fbb0a5
Wiaz24 <wiazhub@gmail.com>
2024-10-29 12:20:55 +0100

    add backeng cert to known certs

--- angular-alledrogo-lx/docker-entrypoint.sh
+++ angular-alledrogo-lx/docker-entrypoint.sh
@@ -9,6 +9,7 @@ echo "BACKEND_URL='$BACKEND_URL'"
 # sed -i "s|COGNITO_LOGIN_URL|$COGNITO_LOGIN_URL|g" /usr/share/nginx/html/main*.js
 perl -pi -e "s|COGNITO_LOGIN_URL|$COGNITO_LOGIN_URL|g" /usr/share/nginx/html/main*.js
 perl -pi -e "s|BACKEND_URL|$BACKEND_URL|g" /usr/share/nginx/html/main*.js
+perl -pi -e "s|BACKEND_URL|$BACKEND_URL|g" /etc/nginx/conf.d/default.conf
 
 entrypoint_log() {
     if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
